c089abe1f3d8a3b7f9a4c4e2415115e15ffb2f52
Chester Townsend <70181947+ctownse9@users.noreply.github.com>
2023-03-02 19:34:26 -0700

    Update GameWindow.java

--- game/src/GameWindow.java
+++ game/src/GameWindow.java
@@ -17,6 +17,10 @@ private static JPanel grid, LPanel, RPanel;
 private static Tile[] tile;
 private static Tile lastTileClicked = null;
 
+/*
+ * Constructor for game window
+ * Takes a string to set the window name
+ */
 public GameWindow(String s)
 {
  super(s);
@@ -24,26 +28,34 @@ public GameWindow(String s)
  setLayout(gbl);
 }
 
+// Uses actionListener to call button functions
 public void actionPerformed(ActionEvent e) 
 {
  if("Quit".equals(e.getActionCommand()))  
  System.exit(0);
 }
+
+// Method to set up the game board
 public void setUp()
 {
-// Initializing the class variables
+/* Initializing the class variables
+ * Creates a grid for the play area
+ * Creates the left and right panels
+ * Creates an array of 16 tiles
+ */
  GridBagConstraints basic = new GridBagConstraints();
  grid = new JPanel(new GridBagLayout());
  LPanel = new JPanel(new GridBagLayout());
  RPanel = new JPanel(new GridBagLayout());
  tile = new Tile[16];
 
+ // Generates each tile and gives it a number
  for (int i = 0; i < 16; i++)
  {
   tile[i] = new Tile(i);
  }
   
-// Adding the playboxes to the grid panel
+ // Adds playboxes to the board
  basic.ipadx = 75;
  basic.ipady = 75;
  basic.insets = new Insets(0, 0, 0, 0);
@@ -57,24 +69,26 @@ public void setUp()
   }
  }
   
-// Adding the playboxes and tiles to L&R Panels
+ // Adds the playboxes and tiles to L&R Panels
  basic.gridx = 0;
  basic.insets = new Insets(10, 10, 10 ,10);
  for (int i = 0; i < 8; i++)
  {
+  // Left Panel
   basic.gridy = i;
   tile[i] = new Tile(i);
   LPanel.add(new playbox(0,i), basic);
   ((JPanel)LPanel.getComponent(i)).add(tile[i]); 
   ((JPanel)LPanel.getComponent(i)).setBorder(null);
    
+  // Right Panel
   tile[i+8] = new Tile(i+8);
   RPanel.add(new playbox(0,i), basic);
   ((JPanel)RPanel.getComponent(i)).add(tile[i+8]);
   ((JPanel)RPanel.getComponent(i)).setBorder(null);
  }
 
-// Adding grid to the board
+ // Adds grid to the board
  basic.ipadx = 0;
  basic.ipady = 0; 
  basic.gridx = 1;
@@ -82,7 +96,7 @@ public void setUp()
  basic.insets = new Insets(75, 10, 10 ,10);
  add(grid, basic);
   
-// Adding the L&R Panels
+ // Adds the L&R Panels
  basic.ipadx = 1;
  basic.ipady = 1;
  basic.insets = new Insets(1,50,1,50);
@@ -94,7 +108,7 @@ public void setUp()
  basic.gridy = 0;
  add(RPanel, basic);
   
-// Adding the Buttons
+ // Creates space for buttons and adds them
  basic.gridx=1;
  basic.gridy=0;
  basic.ipadx = 0;
@@ -107,13 +121,17 @@ public void setUp()
  return;
  
 }
-// Function to Add the Button
+
+// Function to generate the buttons
 public JPanel addButtons()
 {
  JPanel ButtonPanel = new JPanel();
  JButton[] buttons = {lbutton, mbutton, rbutton};
  String[] action = {"New Game", "Reset", "Quit"};
 
+ /* Goes through array of button names to name each,
+  * gives each a command, and gives it an actionListener
+  */
  for (int i = 0; i < 3; i++)
  {
   buttons[i] = new JButton(action[i]);
@@ -126,34 +144,45 @@ public JPanel addButtons()
  }
  return ButtonPanel;
 }
+
 // Sets lastTileClicked when a tile is clicked
 public static void tileClick(Tile tile)
 {	 
- if (lastTileClicked != null) 
+ if (lastTileClicked != null)
+  // Returns tile clicked before to its original color
   lastTileClicked.setBackground(new Color(175,175,175));
 
+ // Changes the color of the most recently clicked tile
  lastTileClicked = tile;
  lastTileClicked.setBackground(new Color(100, 100, 100));
 }
+
 // Moves the tile to the playbox that was clicked
 public static void playboxClick(playbox pbox) 
 {
  if (lastTileClicked != null) 
  {
   Container parent = lastTileClicked.getParent();
-  if (parent instanceof playbox) 
-  {
-   ((playbox) parent).setBorder(BorderFactory.createLineBorder(Color.BLACK)); // add black border to parent playbox
-   ((playbox) parent).remove(lastTileClicked);
-  }
+   if (parent instanceof playbox) 
+   {
+    //Adds black border to spot tile used to be in
+    ((playbox) parent).setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    ((playbox) parent).remove(lastTileClicked);
+   }
+  
+  //Adds the clicked tile to the layered Jpanel and removes border
   pbox.add(lastTileClicked);
-  pbox.setBorder(null); // remove border from the new playbox
+  pbox.setBorder(null);
+  //Resets the tile's background color
   lastTileClicked.setBackground(new Color(175,175,175));
+  //Resets the tile clicked instance
   lastTileClicked = null;
+  //Redraws or updates all the grids
   grid.repaint();
   LPanel.repaint();
   RPanel.repaint();
  }
+
 }
 
 };
